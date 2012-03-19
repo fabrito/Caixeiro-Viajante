b1e6acec6474c317519f328a9790f98a9591a7c2
Petronio Candido de Lima e Silva <petronio.candido@gmail.com>
2012-03-18 23:24:41 -0300

    - Inclusão do arquivo SistemaRecomendacao.pl  - Conversão da base para utilização de listas

---
+++ Prolog/SistemaRecomendacao.pl
@@ -0,0 +1,42 @@
+% Autor:
+% Data: 18/03/2012
+
+amigos(joao, [jose,maria,gustavo]).
+amigos(gustavo, [welligton,maria,cleyton,clara]).
+amigos(maria, [welligton,gustavo,joao,ivana]).
+amigos(cleyton, [welligton,gustavo]).
+amigos(gustavo, [wallace).
+
+gosta(joao, [futebol,carros,esportes,azul]).
+gosta(maria, [moda,rosa,cinema]).
+gosta(jose, [futebol,literatura,amarelo]).
+gosta(gustavo, [carros,cinema,azul]).
+gosta(welligton, cinema).
+gosta(welligton, literatura).
+gosta(welligton, azul).
+gosta(clara, musica).
+gosta(clara, rosa).
+gosta(clara, esportes).
+gosta(ivana, esportes).
+gosta(ivana, moda).
+gosta(ivana, azul).
+gosta(ivana, cinema).
+gosta(cleyton, futebol).
+gosta(cleyton, musica).
+gosta(cleyton, amarelo).
+comprou(joao,corsa_sedan_azul).
+comprou(joao,camisa_selecao_futebol).
+comprou(maria,vestido_rosa).
+comprou(maria,dvd_opoderosochefao).
+comprou(maria,dvd_setimoselo).
+comprou(jose,camisa_selecao_futebol).
+comprou(jose,livro_cemanosdesolidao).
+comprou(gustavo,corsa_sedan_azul).
+comprou(gustavo,dvd_opoderosochefao).
+comprou(welligton,dvd_opoderosochefao).
+comprou(welligton,livro_codigodavinci). comprou(clara,camisa_selecao_futebol).
+comprou(clara,dvd_lenineaovivo).
+comprou(clara,vestido_rosa).
+comprou(ivana,jaqueta_jeans).
+comprou(cleyton,dvd_caetanoveloso).
+comprou(cleyton,dvd_lenineaovivo).
