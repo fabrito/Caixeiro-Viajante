bf1dc9e5468b9a05343925b351bc04d6da3469a8
Flavio <fabritotec@gmail.com>
2012-06-06 20:23:17 -0300

    caixeiro viajante do dia 6 de junho

--- Heuristicas/Heuristicas/Program.cs
+++ Heuristicas/Heuristicas/Program.cs
@@ -97,7 +97,7 @@ namespace Heuristicas
             viagem.AddComponente(new Acesso("Belo Horizonte", "São Paulo", 586));
             viagem.AddComponente(new Acesso("Belo Horizonte", "Rio de Janeiro", 434));
             viagem.AddComponente(new Acesso("Belo Horizonte", "Curitiba", 1004));
-            viagem.AddComponente(new Acesso("Belo Horizonte", "Joenvile", 1301));
+            viagem.AddComponente(new Acesso("Belo Horizonte", "Florianópoles", 1301));
 
             viagem.AddComponente(new Acesso("Salvador", "Belo Horizonte", 1372));
             viagem.AddComponente(new Acesso("Salvador", "Brasília", 1446));
@@ -105,7 +105,7 @@ namespace Heuristicas
             viagem.AddComponente(new Acesso("Salvador", "São Paulo", 1962));
             viagem.AddComponente(new Acesso("Salvador", "Rio de Janeiro", 1649));
             viagem.AddComponente(new Acesso("Salvador", "Curitiba", 2385));
-            viagem.AddComponente(new Acesso("Salvador", "Joenvile", 2682));
+            viagem.AddComponente(new Acesso("Salvador", "Florianópoles", 2682));
 
             viagem.AddComponente(new Acesso("Brasília", "Belo Horizonte", 716));
             viagem.AddComponente(new Acesso("Brasília", "Salvador", 1446));
@@ -113,7 +113,7 @@ namespace Heuristicas
             viagem.AddComponente(new Acesso("Brasília", "São Paulo", 1015));
             viagem.AddComponente(new Acesso("Brasília", "Rio de Janeiro", 1148));
             viagem.AddComponente(new Acesso("Brasília", "Curitiba", 1366));
-            viagem.AddComponente(new Acesso("Brasília", "Joenvile", 1673));
+            viagem.AddComponente(new Acesso("Brasília", "Florianópoles", 1673));
 
             viagem.AddComponente(new Acesso("Vitória", "Belo Horizonte", 524));
             viagem.AddComponente(new Acesso("Vitória", "Salvador", 1202));
@@ -121,7 +121,7 @@ namespace Heuristicas
             viagem.AddComponente(new Acesso("Vitória", "São Paulo", 882));
             viagem.AddComponente(new Acesso("Vitória", "Rio de Janeiro", 521));
             viagem.AddComponente(new Acesso("Vitória", "Curitiba", 1300));
-            viagem.AddComponente(new Acesso("Vitória", "Joenvile", 1597));
+            viagem.AddComponente(new Acesso("Vitória", "Florianópoles", 1597));
 
             viagem.AddComponente(new Acesso("São Paulo", "Belo Horizonte", 586));
             viagem.AddComponente(new Acesso("São Paulo", "Salvador", 1962));
@@ -129,7 +129,7 @@ namespace Heuristicas
             viagem.AddComponente(new Acesso("São Paulo", "Vitória", 882));
             viagem.AddComponente(new Acesso("São Paulo", "Rio de Janeiro", 429));
             viagem.AddComponente(new Acesso("São Paulo", "Curitiba", 408));
-            viagem.AddComponente(new Acesso("São Paulo", "Joenvile", 705));
+            viagem.AddComponente(new Acesso("São Paulo", "Florianópoles", 705));
 
             viagem.AddComponente(new Acesso("Rio de Janeiro", "Belo Horizonte", 434));
             viagem.AddComponente(new Acesso("Rio de Janeiro", "Salvador", 1649));
@@ -137,7 +137,7 @@ namespace Heuristicas
             viagem.AddComponente(new Acesso("Rio de Janeiro", "Vitória", 521));
             viagem.AddComponente(new Acesso("Rio de Janeiro", "São Paulo", 429));
             viagem.AddComponente(new Acesso("Rio de Janeiro", "Curitiba", 852));
-            viagem.AddComponente(new Acesso("Rio de Janeiro", "Joenvile", 1144));
+            viagem.AddComponente(new Acesso("Rio de Janeiro", "Florianópoles", 1144));
 
             viagem.AddComponente(new Acesso("Curitiba", "Belo Horizonte", 1004));
             viagem.AddComponente(new Acesso("Curitiba", "Salvador", 2385));
@@ -145,21 +145,21 @@ namespace Heuristicas
             viagem.AddComponente(new Acesso("Curitiba", "Vitória", 1300));
             viagem.AddComponente(new Acesso("Curitiba", "São Paulo", 408));
             viagem.AddComponente(new Acesso("Curitiba", "Rio de Janeiro", 852));
-            viagem.AddComponente(new Acesso("Curitiba", "Joenvile", 300));
+            viagem.AddComponente(new Acesso("Curitiba", "Florianópoles", 300));
 
-            viagem.AddComponente(new Acesso("Joenvile", "Belo Horizonte", 1301));
-            viagem.AddComponente(new Acesso("Joenvile", "Salvador", 2682));
-            viagem.AddComponente(new Acesso("Joenvile", "Brasília", 1673));
-            viagem.AddComponente(new Acesso("Joenvile", "Vitória", 1597));
-            viagem.AddComponente(new Acesso("Joenvile", "São Paulo", 705));
-            viagem.AddComponente(new Acesso("Joenvile", "Rio de Janeiro", 1144));
-            viagem.AddComponente(new Acesso("Joenvile", "Curitiba", 300));
+            viagem.AddComponente(new Acesso("Florianópoles", "Belo Horizonte", 1301));
+            viagem.AddComponente(new Acesso("Florianópoles", "Salvador", 2682));
+            viagem.AddComponente(new Acesso("Florianópoles", "Brasília", 1673));
+            viagem.AddComponente(new Acesso("Florianópoles", "Vitória", 1597));
+            viagem.AddComponente(new Acesso("Florianópoles", "São Paulo", 705));
+            viagem.AddComponente(new Acesso("Florianópoles", "Rio de Janeiro", 1144));
+            viagem.AddComponente(new Acesso("Florianópoles", "Curitiba", 300));
 
             viagem.GerarViagem("Belo Horizonte");
 
             
             foreach (Acesso a in viagem.rota)
-                System.Console.WriteLine("Partida: " + a.cidPartida + "  Destino: "+a.cidDestino+"  Distancia: "+a.km+"KM");
+                System.Console.WriteLine("Partida: " + a.cidPartida + "  Destino: "+a.cidDestino+"  Distancia: "+a.km+"KM\n");
 
             System.Console.WriteLine("Distância percorrida: " + viagem.kmPercorridos + "KM");
 
